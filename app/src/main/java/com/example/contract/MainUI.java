6a8e1dbcfaa5726422d22a82842a186ac3a92cb0
ElaineAndrianaHosni <6182001045@student.unpar.ac.id>
2023-01-02 20:57:01 +0700

    login

---
+++ app/src/main/java/com/example/contract/MainUI.java
@@ -0,0 +1,9 @@
+package com.example.contract;
+
+import android.content.Context;
+
+public interface MainUI {
+    public void loginBerhasil(String token);
+    public void loginGagal(String error);
+    public Context getContext();
+}
--- app/src/main/java/com/example/uas_p3b/MainActivity.java
+++ app/src/main/java/com/example/uas_p3b/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.uas_p3b;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.contract.MainUI;
+import com.example.presenter.MainPresenter;
+import com.example.uas_p3b.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity implements MainUI, View.OnClickListener {
+    private ActivityMainBinding binding;
+    private MainPresenter mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding =  ActivityMainBinding.inflate(getLayoutInflater());
+        binding.btnLogin.setOnClickListener(this);
+        mp = new MainPresenter(this);
+        setContentView(binding.getRoot());
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view==binding.btnLogin){
+            mp.Login(binding.etEmail.getText().toString(),binding.etPassword.getText().toString(),binding.etRole.getText().toString());
+        }
+    }
+
+    @Override
+    public void loginBerhasil(String token) {
+        Toast.makeText(this, token, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void loginGagal(String error) {
+        Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public Context getContext() {
+        return this;
     }
 }
